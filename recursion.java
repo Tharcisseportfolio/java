121e4fa023cc44e61c995e823635b599228c3efd
Ntirandekura Tharcisse <119756165+tharcissentirandekura@users.noreply.github.com>
2023-12-25 07:17:52 +0000

    save this now because I am done with it at this moment.

--- recursion.java
+++ recursion.java
@@ -1,19 +1,18 @@
 // fibonnaci sequences
 
 public class recursion {
-    public static int recursive(int n){
-        if (n <=1){
+    public static long recursive(int n){
+        if (n <= 1){
             return n;
-
-        }else{
-            // System.out.println(n);
-            return recursive(n - 1) + recursive(n -2);
         }
-
+        else{
+            return recursive(n - 1) + recursive(n - 2 );
+        }
+        
     }
     public static void main(String[] args) {
         // recursive(5);
-        System.out.println(recursive(5));  
+        System.out.println(recursive(50));    
     }
     
 }
